c1b90886a2571d4240e4f6bcf536ee6d4356b810
Junior Paula <luke.junnior@icloud.com>
2025-01-29 10:09:35 -0300

    Create model for user

---
+++ go.mod
@@ -0,0 +1,5 @@
+module get_pet
+
+go 1.22.4
+
+require golang.org/x/crypto v0.32.0
---
+++ internal/model/user.go
@@ -0,0 +1,27 @@
+package model
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"lastt_name"`
+	Email     string `json:"email" gorm:"unique"`
+	Password  string `json:"-"`
+	IsAdmin   bool   `json:"is_admin"`
+}
+
+func NewUser(firstName, lastName, email, password string, isAdmin bool) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Password:  string(hash),
+		IsAdmin:   isAdmin,
+	}, nil
+}
