56f38620a771b8089fa003e1fa47cf118042437a
Junior Paula <luke.junnior@icloud.com>
2025-01-29 19:29:19 -0300

    Implements auth middleware

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -2,16 +2,28 @@ package router
 
 import (
 	"get_pet/internal/handler"
+	"get_pet/internal/middleware"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
 func BootstrapRouter(app *fiber.App, db *gorm.DB) {
+	api := app.Group("/api")
 
 	userHandler := handler.NewUserHandler(db)
 	authHandler := handler.NewAuthHandler(db)
 
-	app.Post("/register", userHandler.Register)
-	app.Post("/login", authHandler.Login)
+	api.Post("/register", userHandler.Register)
+	api.Post("/login", authHandler.Login)
+
+	userRouter := api.Group("/user", middleware.AuthMiddleware)
+	userRouter.Get("profile", func(c *fiber.Ctx) error {
+		user := c.Locals("user")
+		if user == nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": true, "message": "anauthoried"})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"error": false, "message": "user log in", "data": user})
+	})
 }
